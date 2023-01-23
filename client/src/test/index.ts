779d4f1a7a9167906916e85db1c99a794086f759
Stuart <stuart1230@gmail.com>
2023-01-23 23:48:32 +1030

    style: formatting

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -3,8 +3,8 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 import * as path from 'path';
-import * as Mocha from 'mocha';
-import {glob} from glob
+import { glob } from 'glob';
+import Mocha = require('mocha');
 
 export function run(): Promise<void> {
   // Create the mocha test
