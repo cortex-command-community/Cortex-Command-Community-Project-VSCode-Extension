c263c4adf797cb9d34f7c60a89354f2644c1bae0
Stuart <stuart1230@gmail.com>
2023-01-28 17:10:33 +1030

    build: revise webpack build with example from vscode lsp
    remove root shared webpack config, settings stored in project level for now.

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,24 +1,56 @@
 const { composePlugins, withNx, withWeb } = require('@nrwl/webpack');
 
-const withDefaults = require('../../shared.webpack.config');
 const path = require('path');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withWeb(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
-  const /**@type WebpackConfig*/ merged = withDefaults(
-      /**@type WebpackConfig*/ config
-    );
 
-  merged.context = path.join(__dirname);
-  merged.entry = {
+  config.mode = 'none'; // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  config.target = 'node'; // extensions run in a node context
+  config.node = {
+    __dirname: false, // leave the __dirname-behaviour intact
+  };
+
+  config.resolve = {
+    mainFields: ['module', 'main'],
+    extensions: ['.ts', '.js'], // support ts-files and js-files
+  };
+
+  config.module = {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            // configure TypeScript loader:
+            // * enable sources maps for end-to-end source maps
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                sourceMap: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  };
+  config.externals = {
+    vscode: 'commonjs vscode', // ignored because it doesn't exist
+  };
+
+  config.output.libraryTarget = 'commonjs';
+
+  // yes, really source maps
+  config.devtool = 'source-map';
+
+  config.context = path.join(__dirname);
+  config.entry = {
     extension: './src/extension.ts',
   };
-  merged.output = {
-    filename: 'extension.js',
-    path: path.join(__dirname, 'out'),
-  };
 
-  return merged;
+  return config;
 });
