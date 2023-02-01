701c45c11e32c7eaf71d5d01ac9f202c10387239
Stuart <stuart1230@gmail.com>
2023-02-01 17:13:38 +1030

    refactor: add list of legal file types to shared lib
    Importable arrays of various file extensions the game allows

--- packages/shared/src/lib/Shared.ts
+++ packages/shared/src/lib/Shared.ts
@@ -2,3 +2,11 @@ export function shared(): string {
   return 'shared';
 }
 
+export const imageFileExtensions = ['.bmp', '.png', '.jpg', '.jpeg'];
+export const scriptFileExtensions = ['.ini', '.txt', '.lua', '.cfg'];
+export const soundFileExtensions = ['.wav', '.ogg', '.mp3', '.flac'];
+export const legalFileExtensions = [
+  ...imageFileExtensions,
+  ...scriptFileExtensions,
+  ...soundFileExtensions,
+];
